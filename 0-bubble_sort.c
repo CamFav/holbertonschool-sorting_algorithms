8c1c791c4c45bce92edfeea8008d005e3e9c838c
Meashyshy <camillefvrl@gmail.com>
2023-05-24 02:19:07 -0700

    Betty Coding Style

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,22 +9,22 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    size_t a = 0, z = 0;
-    int tmp;
+	size_t a = 0, z = 0;
+	int tmp;
 
-    if (!array)
-        return;
-    /* iterate throught the array */
-    for (a = 0; a < size; a++) {
-        /* compare adjacent elements */
-        for (z = 0; z < size - a - 1; z++) {
-            /* swap elements */
-            if (array[z] > array[z + 1]) {
-                tmp = array[z];
-                array[z] = array[z + 1];
-                array[z + 1] = tmp;
-                print_array(array, size);
-            }
-        }
-    }
+	if (!array)
+		return;
+	/* iterate throught the array */
+	for (a = 0; a < size; a++) {
+		/* compare adjacent elements */
+		for (z = 0; z < size - a - 1; z++) {
+			/* swap elements */
+			if (array[z] > array[z + 1]) {
+				tmp = array[z];
+				array[z] = array[z + 1];
+				array[z + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
 }
