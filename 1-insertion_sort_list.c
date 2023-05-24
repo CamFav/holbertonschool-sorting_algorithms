f46f6365661795e63f70be0956fea6df313ef8a7
Meashyshy <camillefvrl@gmail.com>
2023-05-24 05:43:20 -0700

    T1 - Checker test

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+
 /**
  * insertion_sort_list - function that sorts a doubly linked list of
  * integers in ascending order using the insertion sort algorithm.
@@ -8,5 +9,29 @@
  */
 void insertion_sort_list(listint_t **list)
 {
+    listint_t *head, *tmp;
 
+    if (!list || !*list)
+        return;
+
+    head = *list;
+    for (; head != NULL; head = head->next)
+    {
+        tmp = head;
+        for (; tmp->prev && tmp->prev->n > tmp->n; tmp = tmp->prev)
+        {
+            tmp->prev->next = tmp->next;
+            if (tmp->next)
+                tmp->next->prev = tmp->prev;
+            tmp->next = tmp->prev;
+            tmp->prev = tmp->prev->prev;
+            tmp->next->prev = tmp;
+            if (tmp->prev)
+                tmp->prev->next = tmp;
+            else
+                *list = tmp;
+
+            print_list(*list);
+        }
+    }
 }
